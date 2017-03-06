7f2ad284f140b2849b047912c07ff8c784c9c243
Leon <526247082@qq.com>
2017-03-06 18:59:57 +0800

    Setup project

---
+++ src/com/leon/demo/MyServlet.java
@@ -0,0 +1,38 @@
+package com.leon.demo;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class MyServlet implements Servlet{
+
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		
+	}
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		response.getWriter().write("Hello Servlet");
+	}
+
+}
