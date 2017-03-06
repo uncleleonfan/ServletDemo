19b69f7969f89fcc770c863f4c4bf272c467b584
Leon <526247082@qq.com>
2017-03-06 19:21:45 +0800

    No need to config in web.xml

--- src/com/leon/demo/MyHttpServlet.java
+++ src/com/leon/demo/MyHttpServlet.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MyHttpServlet
  */
+//No need to config in web.xml
 @WebServlet("/myhttpservlet")
 public class MyHttpServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
